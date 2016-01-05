f38a549a573ed80919c7deab2b72a01c6537981c
Nicholai Goodall <Kranoscorp@gmail.com>
2016-01-05 17:47:44 -0500

    fayebot starts :3

---
+++ README.md
@@ -0,0 +1,2 @@
+# Kitten
+A modular IRC bot for #meowcity at Rizon. Based off of the wonderful Sekshibot by ReAnna
---
+++ app.rb
@@ -0,0 +1,5 @@
+require 'cinch'
+require 'json'
+
+configFile = File.read('config.json')
+Var config = JSON.parse(file)
