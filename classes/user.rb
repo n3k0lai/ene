b8def524913e57e94e280bb3f7e5a0afb628edf3
Nicholai Goodall <Kranoscorp@gmail.com>
2016-01-09 14:40:39 -0500

    user class mostly done

--- classes/user.rb
+++ classes/user.rb
@@ -1,8 +1,16 @@
 
 class User
+  include CouchPotato::Persistence
 
-  def constructor(@name)
-    @created = Time.now.utc
-    @lastspoke = Time.now.utc
-  end
+  poperty :name, :type => String
+  validates_presence_of :name
+  property :created, :type => Date, :default => Proc.new { Time.now.utc }
+  property :lastSpoke, :type => Date, :default => Proc.new { Time.now.utc }
+  property :randomPhrase, :type => String, :default => ''
+  property :wallet, :type => Fixnum, :default => 0
+  property :pseudonym, :type => [String], :default => []
+  property :waifu, :type => String, :default => ''
+  property :husbando, :type => String, :default => ''
+  property :selfie, :type => String, :default => ''
+
 end
