6f179c3e6112767729316ffef0afd85c1585b86d
catlover91 <7215827+CatLover91@users.noreply.github.com>
2017-11-01 18:29:57 +0000

    cant believe I missed that merge. added prelim testers for rice

--- src/app.js
+++ src/app.js
@@ -1,20 +1,13 @@
 import Core from './core/core'
 import config from '../config'
 import TwitterAdapter from './adapters/twitter'
-<<<<<<< HEAD
+
 // import IrcAdapter from './adapters/irc'
+// import DiscordAdapter from './adapters/discord'
 
 let Ene = new Core(config)
 let Twitter = new TwitterAdapter(Ene, config.twitter)
 // let Irc = new IrcAdapter(Ene, config.irc)
-=======
-//import IrcAdapter from './adapters/irc'
-import DiscordAdapter from './adapters/discord'
-
-let Ene = new Core(config);
-let Twitter = new TwitterAdapter(Ene, config.twitter);
-//let Irc = new IrcAdapter(Ene, config.irc)
-let Discord = new DiscordAdapter(Ene, config.discord);
->>>>>>> 7ad87cb26ccf7d4bfb4ea0a78bfa407aa395fd42
+// let Discord = new DiscordAdapter(Ene, config.discord)
 
 Twitter.connect()
--- src/plugins/rice.js
+++ src/plugins/rice.js
@@ -1,20 +1,52 @@
 import Plugin from '../core/plugin.js'
 
-<<<<<<< HEAD
-export default class Rice {
-  static test (str) {
-=======
 export default class Rice extends Plugin {
-  constructor() {
 
-  }
-
-  static test(str) {
->>>>>>> 7ad87cb26ccf7d4bfb4ea0a78bfa407aa395fd42
+  static test (str) {
     return str.includes('add')
   }
 
   static respond (str) {
     return 'added'
+  }
+
+  static testAdd (str) {
+    return str.includes('add')
+  }
+
+  static testRm (str) {
+    return str.includes('rm') || str.includes('rem') || str.includes('del')
+  }
+
+  static testHome (str) {
+    return str.includes('home') || str.includes('homescreen') || str.includes('homescreens')
+  }
+
+  static testDtop (str) {
+    return str.includes('desk') || str.includes('desktop') || str.includes('dtop')
+  }
+
+  static testWaifu (str) {
+    return str.includes('waifu')
+  }
+
+  static testHusbando (str) {
+    return str.includes('husbando')
+  }
+
+  static testBattleStation (str) {
+    return str.includes('battlestation') || str.includes('bullshit') || str.includes('bs')
+  }
+
+  static testMemer (str) {
+    return str.includes(':^)')
+  }
+
+  static testQ (str) {
+    return str.includes('??')
+  }
+
+  static testNp (str) {
+    return str.includes('np') || str.includes('playing')
   }
 }
