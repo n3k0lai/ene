6731514bbffba79c03a3becd00c4b8ae5a4c1ed0
nicholai <7215827+n3k0lai@users.noreply.github.com>
2023-03-17 00:08:38 -0400

    nuke most of everything

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/n3k0lai/ene
+
+go 1.19
+
+require (
+	github.com/nicklaw5/helix/v2 v2.21.0
+)
+
+require (
+)
--- src/app.js
+++
@@ -1,8 +0,0 @@
-import Core from './core/core'
-import config from '../config'
-import TwitterAdapter from './twitter'
-
-let Ene = new Core(config)
-let Twitter = new TwitterAdapter(Ene, config.twitter)
-
-Twitter.connect()
